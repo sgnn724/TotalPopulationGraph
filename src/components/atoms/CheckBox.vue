b3b12b1c80daf1c9b5b2c1a195697d74d1d8bbc2
菅原那菜 <lea.suga24@gmail.com>
2023-10-01 16:38:44 +0900

    feat:CheckBoxコンポーネント

---
+++ src/components/atoms/CheckBox.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue'
+import { ref } from 'vue'
+
+const props = defineProps({
+  label: String,
+  modelValue: Boolean
+})
+const label = props.label
+const isChecked = ref(props.modelValue)
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <label class="label">
+    <input
+      type="checkbox"
+      v-model="isChecked"
+    />
+    {{ label }}
+  </label>
+</template>
