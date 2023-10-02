c6c1b146dbb63a2075c90092ff0c89a2e2f12d2a
菅原那菜 <lea.suga24@gmail.com>
2023-10-02 22:45:32 +0900

    feat:app画面表示

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import PrefecturesList from '@/components/organisms/PrefecturesList.vue'
+</script>
+
+<template>
+  <h2>都道府県別の総人口推移グラフ</h2>
+  <p>都道府県</p>
+  <Suspense>
+    <template #default>
+      <PrefecturesList />
+    </template>
+    <template #fallback>
+      <p>Loading...</p>
+    </template>
+    <template #error>
+      <p>エラーが発生しました</p>
+    </template>
+  </Suspense>
+</template>
