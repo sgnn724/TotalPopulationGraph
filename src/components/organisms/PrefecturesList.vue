7944adbf7982983e404b4c6217a088be3545aaf1
菅原那菜 <lea.suga24@gmail.com>
2023-10-02 22:43:44 +0900

    feat:都道府県チェックリスト

---
+++ src/components/organisms/PrefecturesList.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import CheckBox from '../atoms/CheckBox.vue'
+import { RepositoryFactory } from '@/repositories/RepositoryFactory'
+
+const prefecturesRepository = RepositoryFactory.get('prefectures')
+const checkboxItems = ref([])
+const data = await prefecturesRepository.get()
+checkboxItems.value = data.result
+</script>
+
+<template>
+  <div class="container">
+    <CheckBox
+      v-for="(item, index) in checkboxItems"
+      :key="index"
+      :label="item.prefName"
+      v-model:modelValue="item.isChecked"
+    />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+</style>
