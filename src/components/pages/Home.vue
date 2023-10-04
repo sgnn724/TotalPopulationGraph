239138b4df2616d664dee86f151b8f056e66e52e
菅原那菜 <lea.suga24@gmail.com>
2023-10-04 23:07:51 +0900

    update:変数名修正

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -2,11 +2,11 @@
 import { ref } from 'vue'
 import PrefecturesList from '../organisms/PrefecturesList.vue'
 
-const multiChecked = ref([])
+const checkedValue = ref([])
 </script>
 
 <template>
   <h2>都道府県別の総人口推移グラフ</h2>
   <p>都道府県</p>
-  <PrefecturesList v-model="multiChecked" />
+  <PrefecturesList v-model="checkedValue" />
 </template>
