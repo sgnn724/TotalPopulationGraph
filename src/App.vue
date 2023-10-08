e5188d81452967e7069ca6d58e2af83fd2ef4083
菅原那菜 <lea.suga24@gmail.com>
2023-10-08 21:39:59 +0900

    update:pages/home作成

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
-import PrefecturesList from '@/components/organisms/PrefecturesList.vue'
+import Home from '@/components/pages/Home.vue'
 </script>
 
 <template>
-  <h2>都道府県別の総人口推移グラフ</h2>
-  <p>都道府県</p>
   <Suspense>
     <template #default>
-      <PrefecturesList />
+      <Home />
     </template>
     <template #fallback>
       <p>Loading...</p>
