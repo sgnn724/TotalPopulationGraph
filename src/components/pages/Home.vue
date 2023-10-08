d111b71b3ea9a453f95653c21b6bd33f6e638839
菅原那菜 <lea.suga24@gmail.com>
2023-10-08 17:33:34 +0900

    update:チェックした都道府県の総人口取得

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,12 +1,55 @@
 <script setup>
-import { ref } from 'vue'
-import PrefecturesList from '../organisms/PrefecturesList.vue'
+import { reactive, ref } from 'vue'
+import { RepositoryFactory } from '@/repositories/RepositoryFactory'
+import PrefecturesList from '@/components/organisms/PrefecturesList.vue'
+import PopulationChart from '@/components/organisms/PopulationChart.vue'
 
-const checkedValue = ref([])
+const chartData = reactive({
+  populations: [],
+  years: []
+})
+const boundaryYear = ref('')
+
+const populationRepository = RepositoryFactory.get('population')
+const getPopulation = async ({ prefCode, prefName }) => {
+  const data = await populationRepository.get({
+    prefCode,
+    cityCode: '-'
+  })
+  boundaryYear.value = data.result.boundaryYear
+  addChartData(prefName, data.result.data[0].data)
+}
+
+const addChartData = (name, data) => {
+  const actualValue = []
+  data.forEach((value) => {
+    if (value.year <= boundaryYear.value) {
+      actualValue.push(value.value)
+      if (!chartData.years.includes(value.year)) {
+        chartData.years.push(value.year)
+      }
+    }
+  })
+  const population = {
+    name,
+    data: actualValue
+  }
+  chartData.populations.push(population)
+}
+
+const uncheckedPref = ({ prefName }) => {
+  chartData.populations = chartData.populations.filter(
+    (value) => value.name !== prefName
+  )
+}
 </script>
 
 <template>
   <h2>都道府県別の総人口推移グラフ</h2>
   <p>都道府県</p>
-  <PrefecturesList v-model="checkedValue" />
+  <PrefecturesList
+    :getPopulation="getPopulation"
+    :uncheckedPref="uncheckedPref"
+  />
+  <PopulationChart :chartData="chartData" />
 </template>
